d36e24a0e47f931769a0f698a251c31d53e06868
victor <victor@original.prunkster>
2012-10-11 04:25:08 +0400

    Initial commit with Russian and English configurations.

---
+++ pysoundex.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+version = '0.1'
+name = "Simple multilanguage soundex"
+author = "Victor Kuriashkin"
+description = "You can modify lang_soundconfig file for adding new language. Initial contains English and Russian"
+
+
+from  lang_soundconfig import soundconfig
+import re
+
+def soundex(word, lang="en_US"):
+    "Really simple soundex realisation. Multilanguage - configurable"
+    if len(word)==0: return "0000"
+    
+    cfg = soundconfig[lang]
+    word = word.decode("utf-8").lower()    
+    snd_arr = [word[0],0,0,0]
+    
+    word = word[1:]    
+    word = re.sub('[%s]' % ''.join(cfg["vowels"]), '', word)
+    i=1
+    for c in word:
+        if cfg["codes"].has_key(c):
+            char_code = cfg["codes"][c]
+            if snd_arr[i-1] != char_code:
+                snd_arr[i] = char_code
+                i += 1
+            if i==4: break
+    return ''.join(map(lambda x: unicode(x), snd_arr))
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysoundex',
+      version='1.0',
+      description='Multilanguage configurable Soundex',
+      author='Victor Kuriashkin',
+      author_email='vk4arm@gmail.com',
+      py_modules=['pysoundex', 'lang_soundconfig'],
+      url='http://vk4arm.blogspot.com/2012/10/multilanguage-soundex-algorithm-and.html'
+      
+     )
